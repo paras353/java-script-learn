23b41b87266c6cd730f71680806c707ed4334eaf
paras353 <117708471+paras353@users.noreply.github.com>
2025-07-17 08:17:51 +0000

    data types done

---
+++ basic-01/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+//alert("hellow"); we are using node js not browser
+
+let name = "Name";
+let age = 30;
+let isLoggedIn = true;
+
+// Number 45
+// bigInt 234567890
+// String "Hello" 
+// Boolean  - True and False
+// Null  -  Standalone 
+// Undefiend 
+// Symbol  - Unique
+// Object 
+
+console.log(typeof undefiend);
